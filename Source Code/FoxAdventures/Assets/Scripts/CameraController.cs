0946ef00efda58f4e1a496f30a7cbcfbe6b11620
kutiekat <kutiekat0318@gmail.com>
2020-06-15 20:55:18 +0700

    Update camera behaviour

---
+++ Source Code/FoxAdventures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Transform farBackground, middleBackground;
+    public float minHeight;
+    public float maxHeight;
+    private Vector2 lastPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, Mathf.Clamp(target.position.y, minHeight, maxHeight), transform.position.z);
+        Vector2 amountToMove = new Vector2(transform.position.x - lastPosition.x, transform.position.y - lastPosition.y);
+
+        farBackground.position += new Vector3(amountToMove.x, amountToMove.y, 0f);
+        middleBackground.position += new Vector3(amountToMove.x, amountToMove.y, 0f) * 0.5f;
+
+        lastPosition = transform.position;
+    }
+}
